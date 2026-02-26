e941d4ef49aba85ef9850d42e4ba2403aeba9c21
Francesco Casetta <francesco.casetta@gmail.com>
2026-02-26 18:49:17 +0100

    feat: create and open file

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "common.h"
+#include "file.h"
 
 void print_usage(char *argv[]) {
   printf("Usage: %s -n -f <database file>\n", argv[0]);
@@ -12,6 +16,8 @@ int main(int argc, char *argv[]) {
   bool newfile = false;
   char *filepath = NULL;
   int c;
+
+  int dbfd = -1;
 
   while ((c = getopt(argc, argv, "nf:")) != -1) {
     switch(c) {
@@ -37,6 +43,22 @@ int main(int argc, char *argv[]) {
     print_usage(argv);
     return 0;
   }
+
+  if (newfile) {
+    dbfd = create_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("unable to create db file.\n");
+      return -1;
+    }
+  } else {
+    dbfd = open_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("unable to open db file.\n");
+      return -1;
+    }
+  }
+
+  close(dbfd);
 
   printf("Newfile: %d\n", newfile);
   printf("Filepath: %s\n", filepath);
